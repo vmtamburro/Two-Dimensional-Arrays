b3dabd252bf9fe7fe07421480041d4ee0e8cf20a
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2018-11-07 20:34:54 -0500

    Add files via upload
    Corrected some errors in input validation.

---
+++ Lab 7 Two Dimensional Arrays/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "arrayPrototype.h"
+
+
+using namespace std;
+
+int main()
+{
+getTotal(numbers);
+getAverage(numbers);
+
+//user input for Row Total
+int row;
+cout << "Enter the number of the row you would like to sum: ";
+cin >> row;
+getRowTotal(numbers, row);
+
+int col;
+
+cout << "Enter the column you would like to sum: ";
+cin >> col;
+getColTotal(numbers, col);
+
+cout << "Enter the row where you would like to find the maximum: ";
+cin >> row;
+getHighestInRow(numbers, row);
+
+
+cout << "Enter the row where you would like to find the minimum: ";
+cin >> row;
+getLowestInRow(numbers, row);
+
+
+}
